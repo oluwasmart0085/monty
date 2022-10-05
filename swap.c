25e616ba902cc9a893353a6d64b86d905ea878e3
oluwasmart <lebioluwasoji@gmail.com>
2022-10-05 05:45:21 -0700

    add swap

---
+++ swap.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * op_swap - Swap the top two elements of the stack
+ * @stack: The stack
+ * @line_number: The line number
+ */
+void op_swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack && (*stack)->next)
+	{
+		temp = (*stack)->n;
+		(*stack)->n = (*stack)->next->n;
+		(*stack)->next->n = temp;
+	}
+	else
+	{
+		dprintf(STDERR_FILENO,
+			"L%u: can't swap, stack too short\n", line_number);
+		free_all(stack);
+		exit(EXIT_FAILURE);
+	}
+}
